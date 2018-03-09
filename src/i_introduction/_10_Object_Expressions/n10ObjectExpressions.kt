2e64dd71a00af9dee7b1cb68bc83bb2a11d7c004
Gökhan Alıcı <gokhanaliccii@gmail.com>
2018-03-09 08:27:03 +0200

    Introduction part completed

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -5,7 +5,7 @@ import util.doc10
 import java.util.*
 
 fun todoTask10(): Nothing = TODO(
-    """
+        """
         Task 10.
         Read about object expressions that play the same role in Kotlin as anonymous classes in Java.
 
@@ -13,11 +13,21 @@ fun todoTask10(): Nothing = TODO(
         In Kotlin you use Kotlin library extensions instead of java.util.Collections,
         but this example is still a good demonstration of mixing Kotlin and Java code.
     """,
-    documentation = doc10()
+        documentation = doc10()
 )
+
+
+object comparator : Comparator<Int> {
+    override fun compare(o1: Int?, o2: Int?): Int {
+        if (o1 != null && o2 != null)
+            return o2 - o1
+        else return 0
+    }
+
+}
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    Collections.sort(arrayList, comparator)
     return arrayList
 }
--- src/i_introduction/_11_SAM_Conversions/n11SAMConversions.kt
+++ src/i_introduction/_11_SAM_Conversions/n11SAMConversions.kt
@@ -3,18 +3,19 @@ package i_introduction._11_SAM_Conversions
 import util.TODO
 import util.doc11
 import java.util.*
+import kotlin.Comparator
 
 fun todoTask11(): Nothing = TODO(
-    """
+        """
         Task 11.
         When an object implements a SAM interface (one with a Single Abstract Method), you can pass a lambda instead.
         Rewrite the previous example changing an object expression to a lambda.
     """,
-    documentation = doc11()
+        documentation = doc11()
 )
 
 fun task11(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, { x, y -> todoTask11() })
+    Collections.sort(arrayList, { x, y -> if (x == null && y == null) 0 else y - x })
     return arrayList
 }
