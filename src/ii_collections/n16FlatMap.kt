a74d8e93d22f768983933bb0ae055b8c6cd4bf23
Gökhan Alıcı <gokhanaliccii@gmail.com>
2018-03-11 19:24:32 +0200

    Collection part 13-14-15-16 sample completed

--- src/ii_collections/n16FlatMap.kt
+++ src/ii_collections/n16FlatMap.kt
@@ -1,5 +1,7 @@
 package ii_collections
 
+import java.util.*
+
 fun example() {
 
     val result = listOf("abc", "12").flatMap { it.toList() }
@@ -7,12 +9,31 @@ fun example() {
     result == listOf('a', 'b', 'c', '1', '2')
 }
 
-val Customer.orderedProducts: Set<Product> get() {
-    // Return all products this customer has ordered
-    todoCollectionTask()
-}
+val Customer.orderedProducts: Set<Product>
+    get() {
+        val flatMap = this.orders.flatMap { it.products }
+        return flatMap.toSet()
+    }
 
-val Shop.allOrderedProducts: Set<Product> get() {
-    // Return all products that were ordered by at least one customer
-    todoCollectionTask()
+val Shop.allOrderedProducts: Set<Product>
+    get() {
+        // Return all products that were ordered by at least one customer
+        todoCollectionTask()
+    }
+
+
+fun main(ars: Array<String>) {
+
+    class Person<T> {
+
+        fun process(a: T) {
+            print(a?.hashCode())
+
+            print("called")
+        }
+    }
+
+    fun <T> isA(value: Any) = value is T
+
+
 }
